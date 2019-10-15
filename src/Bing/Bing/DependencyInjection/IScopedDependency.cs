8f1a2d5ab78e77a36096d253ba7255aaf09b621d
jian玄冰 <jianxuanhuo1@126.com>
2019-10-15 19:44:55 +0800

    feat: 新增注入基类

---
+++ src/Bing/Bing/DependencyInjection/IScopedDependency.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bing.DependencyInjection
+{
+    /// <summary>
+    /// 实现此接口的类型将注册为<see cref="ServiceLifetime.Scoped"/>模式
+    /// </summary>
+    public interface IScopedDependency
+    {
+    }
+}
