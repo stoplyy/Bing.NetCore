1b9f25ca3d1fbb9fb0d9b8309504c7354889d9c8
jian玄冰 <jianxuanhuo1@126.com>
2019-11-11 00:23:43 +0800

    fix: 新增扩展

---
+++ src/Bing.Datas.EntityFramework/Microsoft/EntityFrameworkCore/QueryableExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    /// <summary>
+    /// <see cref="IQueryable{T}"/> 扩展
+    /// </summary>
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// 根据条件包含指定属性
+        /// </summary>
+        /// <typeparam name="TEntity">实体类型</typeparam>
+        /// <typeparam name="TProperty">属性类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <param name="condition">判断条件，该值为true时添加查询条件，否则忽略</param>
+        /// <param name="property">属性表达式</param>
+        public static IQueryable<TEntity> IncludeIfNeed<TEntity, TProperty>(this IQueryable<TEntity> source,
+            bool condition, Expression<Func<TEntity, TProperty>> property) where TEntity : class =>
+            condition ? source.Include(property) : source;
+
+        /// <summary>
+        /// 根据条件包含指定属性
+        /// </summary>
+        /// <typeparam name="TEntity">实体类型</typeparam>
+        /// <typeparam name="TProperty">属性类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <param name="condition">判断条件，该值为true时添加查询条件，否则忽略</param>
+        /// <param name="property">属性表达式</param>
+        public static IQueryable<TEntity> IncludeIfNeed<TEntity, TProperty>(this IQueryable<TEntity> source,
+            Func<bool> condition, Expression<Func<TEntity, TProperty>> property) where TEntity : class =>
+            condition() ? source.Include(property) : source;
+    }
+}
