e6e7725910565f864dc9e0dc5d847ae536b4082c
jian玄冰 <jianxuanhuo1@126.com>
2020-03-03 18:09:45 +0800

    fix: 新增 拦截器提供程序

---
+++ src/Bing.Abstractions/Bing/Aspects/IInterceptorProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Bing.Aspects
+{
+    /// <summary>
+    /// 拦截器提供程序
+    /// </summary>
+    public interface IInterceptorProvider
+    {
+        /// <summary>
+        /// 拦截器类型
+        /// </summary>
+        Type InterceptorType { get; }
+
+        /// <summary>
+        /// 能否拦截
+        /// </summary>
+        /// <param name="method">方法信息</param>
+        bool CanIntercept(MethodInfo method);
+    }
+}
---
+++ src/Bing/Bing/Aspects/InterceptorProviderAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bing.Aspects
+{
+    /// <summary>
+    /// 拦截器提供程序 特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public class InterceptorProviderAttribute : Attribute
+    {
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public Type Type { get; private set; }
+
+        /// <summary>
+        /// 初始化一个<see cref="InterceptorProviderAttribute"/>类型的实例
+        /// </summary>
+        /// <param name="type">拦截器提供程序类型</param>
+        public InterceptorProviderAttribute(Type type)
+        {
+            if (!typeof(IInterceptorProvider).IsAssignableFrom(type))
+                throw new ArgumentException($"Type '{type.FullName}' is not from interface {nameof(IInterceptorProvider)}");
+            Type = type;
+        }
+    }
+}
